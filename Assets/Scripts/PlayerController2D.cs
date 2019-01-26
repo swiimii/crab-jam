5060d5afccd39362c95124fe547335e10b7b90c7
ZakFahey <z.fahey@gmail.com>
2019-01-26 15:42:48 -0500

    Abstracted out crab behavior to different classes

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -8,97 +8,46 @@ public class PlayerController2D : MonoBehaviour
     public Camera cam;
     public Sprite tucked, walking, standing;
 
-    private float  baseJumpForce = 80;
+    private float baseJumpForce = 80;
     private bool isTucked = false, isGrounded = true;
+
+    private CrabBehavior behavior;
 
     void Start()
     {
+        ChangeBehavior(new RollyCrab(this));
     }
 
     // Update is called once per frame
     void Update()
     {
-        //make camera follow player WITHOUT rotating during a tuck
-        //cam.transform.position.Set(this.transform.position.x, this.transform.position.y, Camera.main.transform.position.z);
-
-        if (!isTucked)
+        behavior.OnUpdate();
+        if(Input.GetAxis("Horizontal") > 0)
         {
-            
-             var move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-             transform.position += move * moveSpeed * Time.deltaTime;
-            
-            //GetComponent<Rigidbody2D>().AddForce(new Vector2(moveSpeed * Input.GetAxis("Horizontal"), 0));
-            if (GetComponent<SpriteRenderer>().sprite == walking)
-            {
-
-                GetComponent<Animator>().speed = Mathf.Abs(Input.GetAxis("Horizontal"));
-            }
-
-            if (Input.GetAxis("Horizontal") > 0)
-            {
-                GetComponent<SpriteRenderer>().flipX = false;
-
-            }
-            else if (Input.GetAxis("Horizontal") < 0)
-            {
-                GetComponent<SpriteRenderer>().flipX = true;
-            }
-
-            if (Input.GetButton("Jump") && isGrounded == true)
-            {
-                Jump();
-            }
-
-            if (Input.GetAxis("Vertical") < 0)
-            {
-                Tuck();
-            }
+            behavior.OnMoveRight();
         }
-
-        else if (isTucked)
-        { 
-
-            if (Input.GetButton("Jump") && isGrounded == true)
-            {
-                Jump();
-            }
-            else if (Input.GetAxis("Vertical") > 0)
-            {
-                
-                 UnTuck();
-                
-            }
+        else if(Input.GetAxis("Horizontal") < 0)
+        {
+            behavior.OnMoveLeft();
+        }
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            behavior.OnPressUp();
+        }
+        else if(Input.GetAxis("Vertical") < 0)
+        {
+            behavior.OnPressDown();
+        }
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            behavior.OnJump();
         }
     }
-
-
-    void Jump()
+    
+    public void Jump()
     {
         GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpHeight * baseJumpForce);
         isGrounded = false;
-        //print("Jump");
-    }
-
-    void Tuck()
-    {
-
-        GetComponent<Animator>().enabled = false;
-        GetComponent<SpriteRenderer>().sprite = tucked;
-        var rb = GetComponent<Rigidbody2D>();
-        isTucked = true;
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y);
-        GetComponent<Rigidbody2D>().freezeRotation = false;
-    }
-
-    void UnTuck()
-    {
-        GetComponent<Animator>().enabled = true;
-        GetComponent<SpriteRenderer>().sprite = walking;
-        GetComponent<Transform>().SetPositionAndRotation(transform.position, Quaternion.identity);
-        var rb = GetComponent<Rigidbody2D>();
-        rb.freezeRotation = true;
-        rb.velocity = new Vector2(rb.velocity.x * 0.3f, rb.velocity.y * 0.3f);
-        isTucked = false;
     }
 
 
@@ -108,7 +57,6 @@ public class PlayerController2D : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             isGrounded = true;
-
         }
     }
 
@@ -119,9 +67,10 @@ public class PlayerController2D : MonoBehaviour
         }
     }
 
-
-
-
+    public void ChangeBehavior(CrabBehavior behavior)
+    {
+        this.behavior = behavior;
+    }
 }
         
     
