0551d54f64c8204ebde5b9a791b5153ac8858d65
Sam Scherer <swiimii962@gmail.com>
2019-01-27 14:24:48 -0500

    Added some sound
    Added several sounds, and added sound to the wind effect which blows the trash during the alley scene

---
+++ Assets/Scripts/FinishLineController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLineController : MonoBehaviour
+{
+    public int nextLevel;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
+    }
+}
