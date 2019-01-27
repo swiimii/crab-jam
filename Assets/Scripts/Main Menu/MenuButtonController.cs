72cc319ac27416c49291cfc738832ff6869c932e
Sam Scherer <swiimii962@gmail.com>
2019-01-26 22:58:42 -0500

    Created stand-in main menu

---
+++ Assets/Scripts/Main Menu/MenuButtonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public void BeginGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SelectLevel()
+    {
+        //select level screen
+    }
+}
